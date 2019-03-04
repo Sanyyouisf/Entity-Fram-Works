3b7b279273d226ce8dac4d728eae3819503481d3
Sanyyouisf <sanymamdoh@yahoo.com>
2019-03-04 08:10:24 -0600

    first commit

---
+++ CodeApproach/EntityFrameWork-CodeApproach/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWork_CodeApproach
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
